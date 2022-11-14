698d927463d070ec9ee91485c2c2466b61e2a582
Ben <me@codemonkey.live>
2022-11-14 22:27:49 +0000

    Forgot to commit...

--- FixaScrew.DataSourceAgg.Api/Extensions/StartupConfig.cs
+++ FixaScrew.DataSourceAgg.Api/Extensions/StartupConfig.cs
@@ -1,15 +1,30 @@
-﻿using FixaScrew.DataSourceAgg.Common.Contexts;
+﻿using System.IO.Compression;
+using FixaScrew.DataSourceAgg.Common.Contexts;
 using FixaScrew.DataSourceAgg.Common.Models;
 using FixaScrew.DataSourceAgg.Services;
 using FixaScrew.DataSourceAgg.Services.CsvFileStore;
 using FixaScrew.DataSourceAgg.Services.DatabaseFileStore;
 using FixaScrew.DataSourceAgg.Services.JsonFileStore;
 using FixaScrew.DataSourceAgg.Services.XmlFileStore;
+using Microsoft.AspNetCore.ResponseCompression;
 
 namespace FixaScrew.DataSourceAgg.Api.Extensions;
 
 public static class StartupConfig
 {
+    public static void AddCompression(this IServiceCollection services)
+    {
+        services.Configure<GzipCompressionProviderOptions>(options =>
+        {
+            options.Level = CompressionLevel.SmallestSize;
+        });
+        services.AddResponseCompression(options =>
+        {
+            options.EnableForHttps = true;
+            options.Providers.Add<GzipCompressionProvider>();
+        });
+    }
+    
     public static void AddFileOptions(this IServiceCollection services, WebApplicationBuilder builder)
     {
         services.Configure<DataLocationOptions>(DataLocationOptions.Xml,
