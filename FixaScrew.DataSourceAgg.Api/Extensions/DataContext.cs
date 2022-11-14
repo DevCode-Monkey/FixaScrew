ff98db3c1bc1ecc0318f2724eccbdadc5bbfbf9b
Ben <me@codemonkey.live>
2022-11-14 10:35:39 +0000

    Add SQLLite migration and added to DI container

---
+++ FixaScrew.DataSourceAgg.Api/Extensions/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using FixaScrew.DataSourceAgg.Common.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FixaScrew.DataSourceAgg.Api.Extensions;
+
+public class DataContext : DbContext
+{
+    protected readonly IConfiguration Configuration;
+
+    public DbSet<SQLDataStore> DataStore { get; set; }
+
+    public DataContext(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options.UseSqlite(Configuration.GetConnectionString("SQLDataStore"));
+    }
+}
